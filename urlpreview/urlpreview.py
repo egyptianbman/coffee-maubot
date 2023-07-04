753cd73916a5640deb6e99b346fd4b8a682af875
Beshoy Girgis <shoy@1ds.us>
2023-07-04 10:47:09 -0500

    Fix image handling

--- urlpreview/urlpreview.py
+++ urlpreview/urlpreview.py
@@ -1,4 +1,5 @@
 import mautrix.api
+import re
 from mautrix.types import RoomID, ImageInfo
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
@@ -27,6 +28,8 @@ class UrlpreviewBot(Plugin):
         await evt.mark_read()
         msgs = ""
         count = 0
+        show_image = False
+        filename = uri = ''
         for _, url_str in matches:
           if youtube_pattern.fullmatch(url_str):
             break
@@ -56,6 +59,7 @@ class UrlpreviewBot(Plugin):
             msgs += "> "+str(cont.get('og:site-title', ''))+"\n> "+str(embed_desc)
 
           if cont.get('og:image', False) is not False:
+            embed_img = url_str
             try:
               mauApi = mautrix.api.HTTPAPI('https://matrix.org')
               # download_type='thumbnail' doesn't work because it requires width/height params, but they don't do anything?
@@ -63,14 +67,25 @@ class UrlpreviewBot(Plugin):
               # embed_img = str(mauApi.get_download_url(cont.get('og:image', False), download_type='thumbnail')) + "?width=50&height=50"
             except Exception as e:
               print(e)
-              embed_img = url_str
             finally:
-              msgs += "\n> ### ![[Click to view image >]({})](".format(url_str) + str(embed_img) + ")\n\n"
-          
-          # implement MAX_LINKS
+              response = await self.http.get(embed_img)
+              if response.status != 200:
+                self.log.warning(f"Unexpected status fetching image {url_str}: {response.status}")
+                return None
+              thumbnail = await response.read()
+              filename = url_str + ".jpg"
+              uri = await self.client.upload_media(thumbnail, mime_type='image/jpg', filename=filename)
+              show_image = True
+
           count += 1
 
-        if count <= 0:
-          await evt.react("💨")
+        if count <= 0 or msgs == "":
           return
-        await evt.reply(str(msgs), allow_html=True)
+
+        # print('MSGS', msgs)
+        await evt.respond(str(msgs), allow_html=True)
+
+        if show_image and uri != "" and filename != "":
+          await self.client.send_image(evt.room_id, url=uri, file_name=filename, info=ImageInfo(
+            mimetype='image/jpg'
+          ))
